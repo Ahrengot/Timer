8ae9f818c54c51582cbcdb5de90bcbcfaa1b0bbc
Jens Ahrengot Boddum <ahrengot@gmail.com>
2012-10-06 17:01:53 +0200

    Added logic to retrieve a specific slip from database via /slips/*id*

--- application/controllers/slips.php
+++ application/controllers/slips.php
@@ -18,7 +18,9 @@ class Slips_Controller extends Base_Controller {
     }   
 
 	public function action_show() {
-
+        $id = (int) URI::segment(2);
+        $slip = Slip::find($id);
+        return eloquent_to_json($slip); // eloquent_to_json is better than json_encode because it doesn't wrap single results in an array 
     }    
 
 	public function action_edit() {
